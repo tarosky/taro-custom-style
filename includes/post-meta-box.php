09ad075aaedfbec5077a0685cae5e65e397c79e9
Toru Miki <1564991+waviaei@users.noreply.github.com>
2024-08-29 16:23:57 +0900

    Fix css preview when not yet published.

--- includes/post-meta-box.php
+++ includes/post-meta-box.php
@@ -28,19 +28,26 @@ add_action( 'add_meta_boxes', function( $post_type ) {
 /**
  * Save css
  */
-add_action( 'save_post', function( $post_id, $post ) {
+add_action( 'save_post', function( $post_id, $post, $update ) {
 	if ( ! wp_verify_nonce( filter_input( INPUT_POST, '_tcsnonce' ), 'tcs_post_css' ) ) {
 		return;
 	}
+
 	if ( wp_is_post_revision( $post ) && wp_is_post_autosave( $post ) ) {
-		$key     = '_css_preview';
+		// Post already published, when viewing update preview.
+		$keys    = [ '_css_preview' ];
 		$post_id = $post->post_parent;
 	} elseif ( tcs_post_type_supported( $post->post_type ) ) {
-		$key = '_css';
+		// Post not yet published
+		$keys  = [ '_css_preview', '_css' ];
+		//}
 	} else {
 		// Do nothing.
 		return;
 	}
+
 	// Save CSS
-	update_post_meta( $post_id, $key, filter_input( INPUT_POST, 'tcs-css' ) );
-}, 10, 2 );
+	foreach ( $keys as $key ) {
+		update_post_meta( $post_id, $key, filter_input( INPUT_POST, 'tcs-css' ) );
+	}
+}, 10, 3 );
